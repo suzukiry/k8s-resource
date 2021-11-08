33c3e290a853a9606bb911edbf82105d375df39c
suzukiry <ryo.suzuki8@gmail.com>
2021-11-08 12:29:53 +0900

    Work in progress

--- produer-consumer/src/main/java/jp/rh/kafka/external/routes/ConsumerAPI.java
+++ produer-consumer/src/main/java/jp/rh/kafka/external/routes/ConsumerAPI.java
@@ -22,7 +22,7 @@ public class ConsumerAPI {
 
         Properties props = new Properties();
 //        props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "my-cluster-kafka-bootstrap-myproject.apps.jscholz.rhmw-integrations.net:443");
-        props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "my-cluster-kafka-bootstrap-myproject.apps.jscholz.rhmw-integrations.net:443");
+        props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "routes-cluster-kafka-bootstrap-kafka-routes.apps.cluster-6fqb6.6fqb6.sandbox1092.opentlc.com:443");
 
         props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "external-group");
         props.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
@@ -32,7 +32,7 @@ public class ConsumerAPI {
         props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
 
         props.setProperty(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SSL");
-        props.setProperty(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, "/Users/rsuzuki/lab/project/tmp/cluster.p12");
+        props.setProperty(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, "./cluster.p12");
         props.setProperty(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, "s4Lhg6GU2QeC");
         props.setProperty(SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG, "PKCS12");
 //        props.setProperty(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, "../user.p12");
@@ -44,7 +44,7 @@ public class ConsumerAPI {
          * Create the consumer and subscribe to topics
          */
         KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
-        consumer.subscribe(Pattern.compile("my-topic"));
+        consumer.subscribe(Pattern.compile("kafka-test-apps"));
 
         /*
          * Consume 100 messages or stop when poll returns null
